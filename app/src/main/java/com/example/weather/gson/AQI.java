0a6de576b399945ceaabdbd104d2a66462ea6936
Zhang Donglai <zdlwangyiyouxiang@126.com>
2020-10-30 17:56:17 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/weather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.weather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
